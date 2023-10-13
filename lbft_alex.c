9c0893fb92f3072c51dfd7c51db7da6879b20df2
Alex <alexcrz.pro@gmail.com>
2023-10-13 14:20:11 +0200

    13/10/2023 - 14:19

---
+++ lbft_alex.c
@@ -0,0 +1,78 @@
+//LBLFT - Library by Alex
+//https://devdocs.io/c
+
+//Function : memset
+
+
+//Function : bzero
+
+
+//Function : memcpy
+
+
+//Function : memcopy
+
+
+//Function : memmove
+
+
+//Function : memchr
+
+
+//Function : memcmp
+
+
+//Function : strlen
+#include <wchar.h>
+
+
+//Function : strdup
+
+
+//Function : strcpy
+
+
+//Function : strncpy
+
+
+//Function : strcat
+
+
+//Function : strchr
+
+
+//Function : strrchr
+
+
+//Function : strstr
+
+
+//Function : strnstr
+
+
+//Function : strcmp
+
+
+//Function : strncmp
+
+
+//Function : atoi
+
+
+//Function : isalpha
+
+
+//Function : isalnum
+
+
+//Function : isascii
+
+
+//Function : isprint
+
+
+//Function : toupper
+
+
+//Function : tolower
+
