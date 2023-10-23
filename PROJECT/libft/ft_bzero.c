d2542218fc16603c3719917041af756f3d1df6f5
Alex <alexcrz.pro@gmail.com>
2023-10-23 15:23:31 +0200

    2023-10-23 - update

--- PROJECT/libft/ft_bzero.c
+++ PROJECT/libft/ft_bzero.c
@@ -1,12 +1,12 @@
 // bzero.c
-
+#include "lblft.h"
 
 void bzero(void *s, size_t n)
 {
     int index;
 
     index = 0;
-    while (index <= n)
+    while (index < n)
     {
         ((int*)s)= "\0";
         index ++;
