a4b965e9cb07311a842b2e75b37df76e301aecef
Alex <alexcrz.pro@gmail.com>
2023-10-31 14:01:53 +0100

    2023-10-31 (0)

--- PROJECT/libft/ft_strdup.c
+++ PROJECT/libft/ft_strdup.c
@@ -2,7 +2,26 @@
 
 #include "libft.h"
 
+void *malloc(size_t size)
+{
+    int index = 0;
+    ((int*)memoire)[];
+
+    if(size=0)
+    {
+        int *ptrunique;
+
+        return NULL;
+        // return ptrunique;
+    }
+    else
+    {
+        return memoire[index];
+    }
+}
+
 char *strdup(const char *s)
 {
-    
+
 }
+
