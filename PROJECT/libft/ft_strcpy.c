c4d884ee24e8bec3d51291aa6d32ba0ac5b780c3
Gold_Ayzer <145896385+Red-Ayzer@users.noreply.github.com>
2023-11-14 17:33:18 +0100

    Update ft_strcpy.c

--- PROJECT/libft/ft_strcpy.c
+++ PROJECT/libft/ft_strcpy.c
@@ -1,8 +1,15 @@
-// fonction : strcpy
+// strcpy
 
 #include "libft.h"
 
 char *strcpy(char *dest, const char *src)
 {
-((str)*src)[]
+int index;
+
+(str)*src[index];
+(str)*dest[index]
+(str)*transition[index]
+
+if(strlen((str)*dest[index])<strlen((str)*src[index]))
+
 }
