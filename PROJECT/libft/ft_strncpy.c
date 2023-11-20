80d23d548cc169f2e59ff75080586d2953c52c66
Gold_Ayzer <145896385+Red-Ayzer@users.noreply.github.com>
2023-11-20 13:30:32 +0100

    Update ft_strncpy.c

--- PROJECT/libft/ft_strncpy.c
+++ PROJECT/libft/ft_strncpy.c
@@ -9,8 +9,8 @@ char *strncpy(char *dest, const char *src, size_t n)
     int n;
 
     (str)*src[index];
-    (str)*dest[index]
-    (str)*transition[index]
+    (str)*dest[index];
+    (str)*transition[index];
 
     if(strlen((str)*dest[index]) < strlen((str)*src[index]))
     {
